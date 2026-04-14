65e5e1404458736f21888a1d47fed3dbd4c9474a
Ivoryy06 <spearlized@gmail.com>
2026-04-14 14:13:56 +0700

    Changed UI

--- src/App.jsx
+++ src/App.jsx
@@ -686,23 +686,3 @@ export default function App() {
   );
 }
 
-const [streaks, setStreaks] = userState(null)
-
-useEffect(() => {
-  fetch('/api/streaks')
-  .then(r => r.json())
-  .then(setStreaks)
-}, [])
-
-{streaks && (
-  <div className="streaks">
-    <div>
-      <span>{streaks.current}</span>
-      <label>longest</label>
-    </div>
-    <div>
-      <span>{streak.total_days}</span>
-      <label>days written</label>
-    </div>
-  </div>
-)}
